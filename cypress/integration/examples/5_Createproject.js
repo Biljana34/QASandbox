85256a8fcaa9e2cb30f8225b954bb4f5cb280bdb
Biljana34 <75971641+Biljana34@users.noreply.github.com>
2020-12-17 11:57:57 +0100

    Update 5_Createproject.js

--- cypress/integration/examples/5_Createproject.js
+++ cypress/integration/examples/5_Createproject.js
@@ -11,30 +11,30 @@ it('Log in to QA Sandbox', () => {
 
   cy.get('[data-testid=playground_card_id] > :nth-child(1) > a > .card > span > .card-img-top').click()
 })
-// it('Create project', () => {
+it('Create project', () => {
 
 
-//   cy.contains('CREATE PROJECT').click() 
+  cy.contains('CREATE PROJECT').click() 
 
-//   cy.get('.value').type('Team 1')
+  cy.get('.value').type('Team 1')
 
-//   cy.get('input[placeholder="Select people"]').click()
+  cy.get('input[placeholder="Select people"]').click()
 
-//   cy.get('[aria-label="Mara Maric"]').click()
+  cy.get('[aria-label="Mara Maric"]').click()
 
-//   cy.get('[aria-label="Pero Peric"]').click()
+  cy.get('[aria-label="Pero Peric"]').click()
 
-//   cy.get('[aria-label="Jela Jelic"]').click()
+  cy.get('[aria-label="Jela Jelic"]').click()
 
-//   cy.get('[aria-label="Milos  Milic"]').click()
+  cy.get('[aria-label="Milos  Milic"]').click()
 
-//   cy.get('[aria-label="Maja  Milic"]').click()
+  cy.get('[aria-label="Maja  Milic"]').click()
 
-//   cy.contains('Submit').click()
+  cy.contains('Submit').click()
 
-//   cy.get('[data-testid=playground_card_id] > :nth-child(1) > a > .card > span > .card-img-top').click()
+  cy.get('[data-testid=playground_card_id] > :nth-child(1) > a > .card > span > .card-img-top').click()
 
-// })
+})
 
 it('Edit project', () => {
 
